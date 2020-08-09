2eae18f3de2a7fc3e176cb8daf1f9238aabe363c
ngadiyar93 <nishanth@wisc.edu>
2020-08-09 16:48:38 -0500

    Add a test script

---
+++ Helper functions/testScript.m
@@ -0,0 +1,21 @@
+clear all;
+close all;
+clc;
+
+%% This is a test script that tests if the FEA works okay
+dimensions.t_T = 3e-3;
+dimensions.t_Cu = 4e-3;
+dimensions.t_C = 5e-3;
+dimensions.g = 1e-3;
+dimensions.w_E = 13e-3;
+dimensions.w_C = 9e-3;
+dimensions.d = 4e-3;
+
+winding.N_t = 10;
+winding.gauge = 30;
+winding.I_pk = 10;
+
+settings.meshSize = 0.0005;
+settings.steps = 5;
+
+[losses, force, volume, mass, Ind, Res] = evaluateInductorFEMM(dimensions, winding, settings);
